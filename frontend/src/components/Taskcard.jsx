1ae2c1c37baf4ae6feec10ea7166281f66566741
Rawan634 <rawanosama855@gmail.com>
2025-03-08 22:20:19 +0200

    it was committed

--- frontend/src/components/Taskcard.jsx
+++ frontend/src/components/Taskcard.jsx
@@ -2,12 +2,13 @@ const TaskCard = ({ task }) => {
   return (
     <div className="card shadow-sm p-3 mb-4">
       <div className="card-body">
-        <h5 className="card-title fw-bold">{task.title}</h5>
+        <div className="d-flex justify-content-between">
+          <h5 className="card-title fw-bold">{task.title}</h5>
+          <p className="text-muted mb-0"><strong>Due:</strong> {task.dueDate}</p>
+        </div>
         <p className="card-text">{task.description}</p>
-        <p className="text-muted">Status: {task.status}</p>
-        <p className="text-muted">Priority: {task.priority}</p>
-
-        {/* Edit & Delete Buttons */}
+        <p className="text-muted"><strong>Status:</strong> {task.status}</p>
+        <p className="text-muted"><strong>Priority:</strong> {task.priority}</p>
         <div className="d-flex justify-content-end gap-2 mt-3">
           <button className="btn btn-warning px-3">✏️ Edit</button>
           <button className="btn btn-danger px-3">🗑️ Delete</button>
